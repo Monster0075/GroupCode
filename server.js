3fb17ad27c6b92c482eae66dd011d86d52eef153
Monster0075 <131847438+Monster0075@users.noreply.github.com>
2023-05-04 07:04:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const Server = require('http').createServer(app);
+const io = require('socket.io')(Server);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
@@ -10,15 +10,14 @@ app.get('/', function(req, res) {
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  
-  // Listen for changes to the code
-  socket.on('code-change', (code) => {
-    // Broadcast the updated code to all connected clients
-    socket.broadcast.emit('code-update', code);
-  });
+    console.log('a user connected');
+ 
+    socket.on('ChangedCode', (code) => {
+        socket.broadcast.emit('NewCode', code);
+    });
+
 });
 
-http.listen(3000, () => {
-  console.log('listening on port:3000');
+Server.listen(3000, () => {
+    console.log('Server is running on port:3000');
 });
